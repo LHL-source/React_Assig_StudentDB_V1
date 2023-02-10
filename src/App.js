bbd071497a7591e21e93554c7d3df372bc7e6d13
LHL-source <97887598+LHL-source@users.noreply.github.com>
2023-02-10 17:27:25 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './App.css';
+import {useState} from 'react';
+import BarChart from "./Components/BarChart";
+import Data from "./Data";
+import StudentFilter from "./Components/StudentFilter";
+import LineChart from "./Components/LineChart";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ChartIndividualStudent from "./Components/ChartIndividualStudent";
+
+
+//data.products.map()??
+function App() {
+  const [data]  = useState(Data)
+  const allstudentNames = data.map(students => students.name)
+     console.log("allstudentNames", allstudentNames);
+  const studentNames = [...new Set(allstudentNames)].map((name, index)=> {
+     return {
+          name: name,
+          id: index + 1
+             }
+          
+})//studentNames
+console.log("studentNames:",studentNames);
+//---------------------------------------//
+
+//.map/reduce & weergave van enkele gegeven vanuit bundel van data?
+const assignmentsData = Object.values(Data.reduce((acc, { assignment, difficulty, fun }) => { 
+  acc[assignment] = acc[assignment] || { assignment, difficulty: 0, fun: 0, students: 0 };
+  acc[assignment].difficulty += difficulty;
+  acc[assignment].fun += fun;
+  acc[assignment].students++;
+  return acc;
+}, []))
+//console.log("assignmentsData:"),assigmentsData;
+//---------------------------------------//
+// average per student//
+const averageData = assignmentsData.map(( {assignment, students, difficulty, fun}) => {
+  return {
+     assignment, students, 
+     difficulty: difficulty / students,
+     fun: fun / students
+  }
+  });
+  console.log("averageData:",averageData);
+ //-------------------------------------------// 
+ //console.log average per student//
+ console.log(
+  averageData.filter(({ difficulty, fun }) => {
+    const check = (n) => Number.isNaN() || n === 0;
+    return check(difficulty) || check(fun);
+  })
+);
+
+
+//hieronder return van app//
+  return (                          
+    <div className="App">
+      <Header />
+      
+      <BrowserRouter>
+      <Routes>
+         <Route path='/' element={
+            <div className="main-path">
+               <BarChart data={averageData} />
+               <StudentFilter studentNames={studentNames}/>
+               <LineChart data={averageData}/>
+               </div>
+         }
+         />
+         <Route
+         path='/student/:name' element={
+            <div>
+            <ChartIndividualStudent data ={Data}/>
+            </div>
+         }
+         />
+             </Routes>
+               </BrowserRouter>
+               <Footer/>
+    </div> 
+  );//app return
+}//app
+
+export default App;
---
+++ src/Components/BarChart.js
@@ -0,0 +1,119 @@
+import React from "react"
+
+
+import { 
+    VictoryBar,
+    VictoryChart,
+    VictoryAxis,
+    VictoryTheme,
+    VictoryGroup,
+    VictoryLegend,
+    VictoryLabel,
+    VictoryContainer,
+    VictoryTooltip
+  } from 'victory'
+
+  //Alle opdrachten en waarderingen in een staafdiagram
+
+  function BarChart( {data} ) {
+    const assignmentData = data.map(values => ({
+      assignment: values.assignment,
+      difficulty: values.difficulty,
+      fun: values.fun,
+      label: 
+        `assignment ${values.assignment}, 
+         'difficulty: ${values.difficulty.toFixed(1)},
+         fun: ${values.fun.toFixed(1)}`
+   
+    }))
+    return (
+      <div className="bar-chart">
+       <h1>Gemiddelde resultaten alle studenten</h1>
+        <VictoryChart
+          theme={VictoryTheme.material}
+          width={720}
+          height={200}
+          domain={{x: [0, 55]}}
+          containerComponent={
+          <VictoryContainer width={700} height={250} />
+          }
+        >
+          <VictoryLegend
+          
+            x={550}
+            y={24}
+            itemsPerRow={2}
+            orientation="horizontal"
+            data={[
+                { name: 'Fun', symbol: { fill: "red", type: "square" } },
+                { name: "Difficulty", symbol: { fill: "yellow", type: "square" } },
+            ]}
+          />
+          <VictoryLabel
+            x={50}
+            y={180}
+            text="Assignment"
+            style={[{ fill: "black", fontSize: 10}]}
+          />
+          {/* X-ass*/}
+          <VictoryAxis
+            style={{
+            ticks: { stroke: "grey", size: 2 },
+            tickLabels: {
+                fontSize: 6,
+                padding: 2,
+                angle: -60,
+                textAnchor: "end",
+            },
+            }}
+          />
+          <VictoryLabel
+            x={24}
+            y={80}
+            angle={-90}
+            text="Rating"
+            style={[{ fill: "black", fontSize: 10 }]}
+          />
+        
+            <VictoryAxis
+            style={{
+            ticks: {stroke: "grey", size: 2 },
+            tickLabels: { fontSize: 7, padding: 3 },
+            }}
+            // y:[0, 5]
+            
+            dependentAxis
+          />
+            
+          <VictoryGroup   
+            offset={4}
+            colorScale={["red", "yellow" ]}
+          >
+            <VictoryBar
+              labelComponent=
+              {<VictoryTooltip
+                style={[{ fontSize: 5}]}
+              />}
+              data={assignmentData} 
+              x="assignment" 
+              y="fun"
+              barWidth={3}
+            />
+            <VictoryBar
+              labelComponent=
+              {<VictoryTooltip
+                style={[{ fontSize: 5}]}
+              />}
+              data={assignmentData} 
+              x="assignment" 
+              y="difficulty"
+              barWidth={3}
+            />
+          </VictoryGroup>
+      </VictoryChart>
+    </div>
+    )
+  }
+
+    
+   export default BarChart
